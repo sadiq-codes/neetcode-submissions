f2d6f93a257168796427145ee625ce1730df65ad
Abubakar Muhammad Sadiq <68012668+sadiq-codes@users.noreply.github.com>
2026-07-16 10:47:12 +0100

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        blockDup = [set() for val in range(len(board))]
+        print(blockDup)
+        for row in range(len(board)):
+            rowDup = set()
+            colDup = set()
+            for col in range(len(board[1])):
+                if board[row][col] != "." and board[row][col] in rowDup:
+                    return False
+                rowDup.add(board[row][col])
+
+                if board[col][row] != "." and board[col][row] in colDup:
+                    return False
+                colDup.add(board[col][row])
+
+                blockId = (row // 3) * 3 + (col // 3)
+                if board[row][col] != "." and board[row][col] in blockDup[blockId]:
+                    return False
+                blockDup[blockId].add(board[row][col])
+        return True
+                
+
